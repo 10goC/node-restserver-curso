7a8fd64ca0a6718a4e8f8b16d5a7032d17f72bb8
Diego Curyk <curykdiego@gmail.com>
2020-08-23 02:47:23 -0300

    db

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,8 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ==================
-// Entorno
+// URL conexión BD
 // ==================
-let urlDB = process.env.NODE_ENV === 'dev' ?
+process.env.URLDB = process.env.NODE_ENV === 'dev' ?
     'mongodb://localhost:27017/cafe' :
     'mongodb+srv://curykdiego:dHb2nQAUZHQbMc7Z@cluster0.ueqgg.mongodb.net/cafe?retryWrites=true&w=majority';
-
-process.env.URLDB = urlDB;
