34a82c11168bbb52c2278ba3718e3bc96f6198ce
Diego Curyk <curykdiego@gmail.com>
2020-08-25 01:58:21 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,4 +13,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ==================
 process.env.URLDB = process.env.NODE_ENV === 'dev' ?
     'mongodb://localhost:27017/cafe' :
-    'mongodb+srv://curykdiego:dHb2nQAUZHQbMc7Z@cluster0.ueqgg.mongodb.net/cafe?retryWrites=true&w=majority';
+    process.env.MONGO_URI;
